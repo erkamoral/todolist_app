c9be8bc202acddafa1810f2f5030e44c50b2a677
erkamoral <erkamoral90@gmail.com>
2021-03-24 03:14:00 +0300

    heroku changes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express'),
       path = require('path')
 
 require('dotenv').config()
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 80;
 
 app = express()
 
